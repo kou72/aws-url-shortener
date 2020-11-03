1800ec2f6436531885ca614823226232998d2a3b
kou72 <genpokopokopoko@gmail.com>
2020-11-04 08:45:43 +0900

    ok

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,100 +1,61 @@
-// main.js
 "use strict";
 
-// Initializes MailForm.
-function MailForm() {
+// Initializes
+function UrlShortner() {
   // Shortcuts to DOM Elements.
-  this.nameInput = document.getElementById("name");
-  this.emailInput = document.getElementById("email");
-  this.messageInput = document.getElementById("message");
+  this.input = document.getElementById("input");
+  this.output = document.getElementById("output");
   this.snackbar = document.getElementById("snackbar");
+  this.shortenButton = document.getElementById("shorten");
 
-  // メール送信ボタン
-  this.sendButton = document.getElementById("send");
-  this.sendButton.addEventListener("click", this.sendMessage.bind(this));
-
-  // TODO.17-3
-  // メール保存&送信ボタン
-  // this.saveButton = document.getElementById("save");
-  // this.saveButton.addEventListener("click", this.saveMessage.bind(this));
-
-  // ボタン表示・非表示
+  this.shortenButton.addEventListener("click", this.shortenUrl.bind(this));
   const buttonTogglingHandler = this.toggleButton.bind(this);
-  this.nameInput.addEventListener("change", buttonTogglingHandler);
-  this.nameInput.addEventListener("keyup", buttonTogglingHandler);
-  this.emailInput.addEventListener("change", buttonTogglingHandler);
-  this.emailInput.addEventListener("keyup", buttonTogglingHandler);
-  this.messageInput.addEventListener("change", buttonTogglingHandler);
-  this.messageInput.addEventListener("keyup", buttonTogglingHandler);
+  this.input.addEventListener("change", buttonTogglingHandler);
+  this.input.addEventListener("keyup", buttonTogglingHandler);
 }
 
-// メール送付
-MailForm.prototype.sendMessage = async function (e) {
-  //TODO.15-2
-  // e.preventDefault();
-  // const data = {
-  //   name: this.nameInput.value,
-  //   email: this.emailInput.value,
-  //   message: this.messageInput.value,
-  // };
-  // try {
-  //   await firebase.functions().httpsCallable("sendMail")(data);
-  //   this.successSnackbar();
-  //   this.resetForm();
-  // } catch {
-  //   this.errorSnackbar();
-  // }
-};
+// Shorten the URL
+UrlShortner.prototype.shortenUrl = async function (e) {
+  e.preventDefault();
+  const apiurl = "https://4wstaoc5s6.execute-api.ap-northeast-1.amazonaws.com/post"
+  const param = {
+    method: "POST",
+    mode: "cors",
+    body: this.input.value
+  }
+  try {
+    const response = await fetch(apiurl, param)
+    if (!response.ok) throw "ok propertyi is false"
+    const body = await response.json()
+    console.log(body)
+    this.output.value = body
+    this.successSnackbar();
 
-// メール保存&送付
-MailForm.prototype.saveMessage = async function (e) {
-  //TODO.17-4
-  //   e.preventDefault();
-  //   const ref = firebase.firestore().collection("mailForm");
-  //   try {
-  //     await ref.add({
-  //       name: this.nameInput.value,
-  //       email: this.emailInput.value,
-  //       message: this.messageInput.value,
-  //       timestamp: new Date()
-  //     });
-  //     this.successSnackbar();
-  //     this.resetForm();
-  //   } catch {
-  //     this.errorSnackbar();
-  //   }
-};
-
-MailForm.prototype.toggleButton = function () {
-  if (this.nameInput.value && this.emailInput.value && this.messageInput.value) {
-    this.sendButton.removeAttribute("disabled");
-    // TODO.17-5
-    // this.saveButton.removeAttribute("disabled");
-  } else {
-    this.sendButton.setAttribute("disabled", "true");
-    // TODO.17-6
-    // this.saveButton.setAttribute("disabled", "true");
+  } catch (err) {
+    this.errorSnackbar();
+    console.log(err)
   }
 };
 
-MailForm.prototype.resetForm = function () {
-  this.nameInput.value = "";
-  this.emailInput.value = "";
-  this.messageInput.value = "";
-  this.toggleButton();
+UrlShortner.prototype.toggleButton = function () {
+  if (this.input.value) {
+    this.shortenButton.removeAttribute("disabled");
+  } else {
+    this.shortenButton.setAttribute("disabled", "true");
+  }
 };
 
-MailForm.prototype.successSnackbar = function () {
+UrlShortner.prototype.successSnackbar = function () {
   this.snackbar.style.backgroundColor = "#2196f3";
-  this.snackbar.MaterialSnackbar.showSnackbar({ message: "送信に成功しました" });
+  this.snackbar.MaterialSnackbar.showSnackbar({message: "URLを短縮しました"});
 };
 
-MailForm.prototype.errorSnackbar = function () {
+UrlShortner.prototype.errorSnackbar = function () {
   this.snackbar.style.backgroundColor = "#f44336";
-  this.snackbar.MaterialSnackbar.showSnackbar({ message: "送信に失敗しました" });
+  this.snackbar.MaterialSnackbar.showSnackbar({message: "処理に失敗しました"});
 };
 
 window.onload = function () {
   // Initializes MailForm.
-  window.mailForm = new MailForm();
+  window.UrlShortner = new UrlShortner();
 };
